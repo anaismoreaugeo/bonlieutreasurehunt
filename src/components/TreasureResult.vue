593b7a62a373952c079563d4cbfcd4620bae394d
anaismoreaugeo <66426603+anaismoreaugeo@users.noreply.github.com>
2024-06-20 01:12:59 +0200

    update color and image on treasure

--- src/components/TreasureResult.vue
+++ src/components/TreasureResult.vue
@@ -2,28 +2,47 @@
   <div class="treasure-result">
     <div class="content">
       <div class="center-square">
-        <SliderSquare class="inner-square top-left" />
-        <SliderSquare class="inner-square top-right" />
-        <SliderSquare class="inner-square bottom-left" />
-        <SliderSquare class="inner-square bottom-right" />
+        <SliderSquare class="inner-square top-left" :colorClass="colors[0]" @update-last-clicked="updateLastClicked(0)" />
+        <SliderSquare class="inner-square top-right" :colorClass="colors[1]" @update-last-clicked="updateLastClicked(1)" />
+        <SliderSquare class="inner-square bottom-left" :colorClass="colors[2]" @update-last-clicked="updateLastClicked(2)" />
+        <SliderSquare class="inner-square bottom-right" :colorClass="colors[3]" @update-last-clicked="updateLastClicked(3)" />
       </div>
     </div>
     <div class="bottom-dots">
-      <div class="dot red"></div>
-      <div class="dot blue"></div>
-      <div class="dot yellow"></div>
-      <div class="dot green"></div>
+      <button class="dot red" @click="changeColor('red-stroke')"></button>
+      <button class="dot blue" @click="changeColor('blue-stroke')"></button>
+      <button class="dot yellow" @click="changeColor('yellow-stroke')"></button>
+      <button class="dot green" @click="changeColor('green-stroke')"></button>
     </div>
   </div>
 </template>
 
 <script>
-import SliderSquare from './TreasureSlider/SliderSquare.vue';
+import SliderSquare from './SliderSquare.vue';
 
 export default {
   name: 'TreasureResult',
   components: {
     SliderSquare
+  },
+  data() {
+    return {
+      colors: ['cls-1', 'cls-1', 'cls-1', 'cls-1'],
+      lastClickedIndex: null
+    };
+  },
+  methods: {
+    updateLastClicked(index) {
+      this.lastClickedIndex = index;
+    },
+    changeColor(colorClass) {
+      if (this.lastClickedIndex !== null) {
+        const updatedColors = [...this.colors];
+        updatedColors[this.lastClickedIndex] = colorClass;
+        this.colors = updatedColors;
+        this.lastClickedIndex = null; // Reset last clicked index
+      }
+    }
   }
 };
 </script>
