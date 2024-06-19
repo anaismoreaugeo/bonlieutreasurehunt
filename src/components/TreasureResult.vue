9fab332528d2cbac9aa1092ea746c845b9f8051f
anaismoreaugeo <66426603+anaismoreaugeo@users.noreply.github.com>
2024-06-20 00:46:37 +0200

    Add square slider

--- src/components/TreasureResult.vue
+++ src/components/TreasureResult.vue
@@ -1,13 +1,29 @@
 <template>
-    <div class="treasure-result">
-      <h2>Résultat du Trésor</h2>
-      <p>Ceci est la page des résultats du trésor.</p>
+  <div class="treasure-result">
+    <div class="content">
+      <div class="center-square">
+        <SliderSquare class="inner-square top-left" />
+        <SliderSquare class="inner-square top-right" />
+        <SliderSquare class="inner-square bottom-left" />
+        <SliderSquare class="inner-square bottom-right" />
+      </div>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    name: 'TreasureResult',
-  };
-  </script>
-  
+    <div class="bottom-dots">
+      <div class="dot red"></div>
+      <div class="dot blue"></div>
+      <div class="dot yellow"></div>
+      <div class="dot green"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+import SliderSquare from './TreasureSlider/SliderSquare.vue';
+
+export default {
+  name: 'TreasureResult',
+  components: {
+    SliderSquare
+  }
+};
+</script>
