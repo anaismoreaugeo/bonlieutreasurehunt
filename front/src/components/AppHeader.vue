cc8ad5e554e0b6d11316002a2002f4f95aea0f19
Joyce <lapinette2002@ymail.com>
2024-07-10 22:51:24 +0200

    fix dépassement du logo sur tel

--- front/src/components/AppHeader.vue
+++ front/src/components/AppHeader.vue
@@ -1,13 +1,20 @@
 <template>
-    <header class="header">
-      <img src="@/assets/logo_bonlieu.png" style="height: 100%">
-    </header>
-  </template>
-  
-  <script>
-  export default {
-    name: 'AppHeader',
-  };
-  </script>
- 
-  
+  <header class="header">
+    <img src="@/assets/logo_bonlieu.png" class="logo">
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'AppHeader',
+};
+</script>
+
+<style scoped>
+
+.logo {
+  max-width: 100%;
+  height: auto; 
+  max-height: 100%;
+}
+</style>
